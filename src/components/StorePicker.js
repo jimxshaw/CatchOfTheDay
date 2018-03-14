02544c0125b216c1ecfaafab172f439f43bcb4b3
Jim Shaw <jimxshaw@gmail.com>
2018-03-13 21:01:25 -0500

    Add new App component.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 class StorePicker extends React.Component {
-	render() {
-		return (
-		<Fragment>
-			<form className="store-selector">
-				<h2>Please enter a store</h2>
-				<input type="text" placeholder="Store Name" required />
-				<button type="submit">Visit Store →</button>
-			</form>
-		</Fragment>
-		);
-	}
+  render() {
+    return (
+      <Fragment>
+        <form className="store-selector">
+          <h2>Please enter a store</h2>
+          <input type="text" placeholder="Store Name" required />
+          <button type="submit">Visit Store →</button>
+        </form>
+      </Fragment>
+    );
+  }
 }
 
 export default StorePicker;
