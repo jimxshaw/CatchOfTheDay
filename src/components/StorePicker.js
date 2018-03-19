ccf959e09c2d023957d88bf305712c430d1b7f6d
Jim Shaw <jimxshaw@gmail.com>
2018-03-18 19:51:35 -0500

    Add goToStore property function and bind it to the form's onSubmit handler.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,33 @@
 import React, { Fragment } from "react";
 import { getFunName } from "../helpers";
 
-class StorePicker extends React.Component {
+class StorePicker extends React.Component {  
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // Stop the default behavior, which is 
+    // submitting the form.
+    event.preventDefault();
+
+    // Get the text from the input.
+    console.log(this.myInput);
+
+    // Change the page to /store/userInputText.
+    console.log("Entering the store!");
+  };
+
   render() {
     return (
       <Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
-          <input type="text" placeholder="Store Name" defaultValue={getFunName()} />
+          <input
+            required 
+            type="text"
+            ref={this.myInput} 
+            placeholder="Store Name" 
+            defaultValue={getFunName()} 
+          />
           <button type="submit">Visit Store →</button>
         </form>
       </Fragment>
