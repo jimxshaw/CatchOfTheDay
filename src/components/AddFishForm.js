742dac3e0009f35cfa0f1e70502d8c37ef5b6f9a
Jim Shaw <jimxshaw@gmail.com>
2018-04-07 22:53:29 -0500

    Add the AddFishForm component and use it in Inventory component.

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <p>Add Fish Form</p>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
